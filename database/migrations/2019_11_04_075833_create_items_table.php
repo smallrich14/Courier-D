48a00e7bca1c2929b59681cdbebfa4da8c63290b
Kristhine Orapa <kristhineorapa@gmail.com>
2019-11-04 16:11:38 +0800

    create Category || Items || Status

---
+++ database/migrations/2019_11_04_075833_create_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->float('price');
+            $table->mediumText('description');
+            $table->string('image');
+            $table->unsignedBigInteger('category_id')->nullable(true);
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('statuses')->insert([
+        	'name' => 'available'
+        ]);
+
+        DB::table('statuses')->insert([
+        	'name' => 'not available'
+        ]);
+    }
+}
