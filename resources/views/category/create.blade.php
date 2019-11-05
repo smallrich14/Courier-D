8f7fd5921339cb4f6c51a27347bfafff21eae671
Kristhine Orapa <kristhineorapa@gmail.com>
2019-11-05 09:14:34 +0800

    configure database

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,1 +1,31 @@
+@extends('layouts.app')
 
+@section('content')
+
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-8 mx-auto">
+				<h3 class="text-center">Unit Categories</h3>
+				<hr>
+				<form action="" method="post">
+				@csrf
+					<div class="form-group text-center">
+						<label for="category" >New Category</label>
+						{{-- <div class="pb-3">
+							<input type="name" name="title" class="form-control">
+						</div>
+						<div class="pb-3">
+							<input type="file" name="image" class="file form-control">
+						</div> --}}
+							<input type="text" name="category" class="form-control" 
+								value="{{isset($category) ? $category->name : "" 	}}"
+							>
+					</div>
+						<button type="submit" class="btn btn-info w-100">{{isset($category) ? "Update" : "Add"}}</button>
+				</form>
+			</div>	
+		</div>
+	</div>
+
+
+@endsection
